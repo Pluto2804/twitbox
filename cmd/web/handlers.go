ae228bd49f132f27edb78a91cc23048a7779b7bb
VEDANT SHEKHAR KUGAONKAR <vedant.kugaonkar@gmail.com>
2025-10-08 23:23:23 +0530

    Added functionality to query the database and retrieve the twit according to a specific id using QueryRow and Query ,added a latest function to retrieve and roughly display last 10 twits in descending order

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+
+	"twitbox.vedantkugaonkar.net/internal/model"
 )
 
 func (app *application) home(w http.ResponseWriter, req *http.Request) {
@@ -12,23 +14,30 @@ func (app *application) home(w http.ResponseWriter, req *http.Request) {
 		http.NotFound(w, req)
 		return
 	}
-	files := []string{
-		"ui/html/base.tmpl.html",
-		"ui/html/pages/home.tmpl.html",
-		"ui/html/partials.tmpl.html",
-	}
+	// files := []string{
+	// 	"ui/html/base.tmpl.html",
+	// 	"ui/html/pages/home.tmpl.html",
+	// 	"ui/html/partials.tmpl.html",
+	// }
 
-	fs, err := template.ParseFiles(files...)
+	// fs, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+	// err = fs.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	twits, err := app.twits.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	err = fs.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, err)
-		return
-
+	for _, twit := range twits {
+		fmt.Fprintf(w, "%+v\n", twit)
 	}
+
 }
 func (app *application) twitCreate(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
@@ -36,7 +45,17 @@ func (app *application) twitCreate(w http.ResponseWriter, req *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Fprintln(w, "Create a twit!")
+
+	title := "O snail"
+	content := "O snail\nClimb mount Fuji,\nBut slowly,slowly!\n\n- Kobayashi Issa"
+	expires := 7
+	id, err := app.twits.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	//redirecting user to the relevant page for the twit
+	http.Redirect(w, req, fmt.Sprintf("/twit/view?id=%d", id), http.StatusSeeOther)
 }
 func (app *application) twitView(w http.ResponseWriter, req *http.Request) {
 	id, err := strconv.Atoi(req.URL.Query().Get("id"))
@@ -44,6 +63,15 @@ func (app *application) twitView(w http.ResponseWriter, req *http.Request) {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific twit with an id ....%v", id)
+	twit, err := app.twits.Get(id)
+	if err != nil {
+		if errors.Is(err, model.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%+v", twit)
 
 }
--- internal/model/twits.go
+++ internal/model/twits.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -17,11 +18,53 @@ type TwitModel struct {
 }
 
 func (tm *TwitModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO twits(title,content,created,expires)
+	        VALUES(?,?,UTC_TIMESTAMP(),DATE_ADD(UTC_TIMESTAMP(),INTERVAL ? DAY))`
+	result, err := tm.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+	//LastInsertId() method on the result to get the ID of our
+	//newly inserted record in twits table
+	id, err := result.LastInsertId()
+	return int(id), err
 }
 func (tm *TwitModel) Get(id int) (*Twit, error) {
-	return nil, nil
+	stmt := `SELECT id,title,content,created,expires FROM twits 
+	         WHERE expires > UTC_TIMESTAMP() AND id=?`
+	row := tm.DB.QueryRow(stmt, id)
+	s := &Twit{}
+	err := row.Scan(&s.Id, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return s, err
 }
 func (tm *TwitModel) Latest() ([]*Twit, error) {
-	return nil, nil
+	stmt := `SELECT id,title,content,created,expires FROM twits 
+	      WHERE expires>UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+	rows, err := tm.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	twits := []*Twit{}
+	for rows.Next() {
+		t := &Twit{}
+		err := rows.Scan(&t.Id, &t.Title, &t.Content, &t.Created, &t.Expires)
+		if err != nil {
+			return nil, err
+		}
+		twits = append(twits, t)
+
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return twits, nil
+
 }
